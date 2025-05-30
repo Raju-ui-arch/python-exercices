0159ee7117a816e100e69e15a6d489b5ad1fd086
J Bhagya Raju <jbhagya@essbbsr.esspl.com>
2025-05-30 15:54:20 +0530

    solution-2

---
+++ pythonVersionChecker.py
@@ -0,0 +1,14 @@
+# 2. Python Version Checker
+
+# Write a Python program to find out what version of Python you are using.
+# Click me to see the sample solution.
+
+import sys
+# Function to check Python version
+def check_python_version():
+    version = sys.version
+    print("You are using Python version:", version)
+
+# Call the function to check Python version
+check_python_version()
+# End of the code snippet
